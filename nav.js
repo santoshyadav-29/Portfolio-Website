f746e46544b5d631580321c1c750b813377f33ae
Santosh <sy59021321@student.ku.edu.np>
2023-01-30 12:39:43 +0545

    Website umloaded

---
+++ nav.js
@@ -0,0 +1,9 @@
+ham_icon.addEventListener("click", function toggle() {
+    if(navbar.style.display === "flex") {
+      navbar.style.display = "none";
+    } else {
+      navbar.style.display = "flex";
+    }
+  });
+  
+  
