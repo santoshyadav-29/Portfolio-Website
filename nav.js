f88bca3a7abe05aef032b861385e3242898259fe
Santosh <sy59021321@student.ku.edu.np>
2023-03-27 09:13:26 +0545

    Changed layout little bit

--- nav.js
+++ nav.js
@@ -1,9 +1,15 @@
-ham_icon.addEventListener("click", function toggle() {
-    if(navbar.style.display === "flex") {
-      navbar.style.display = "none";
-    } else {
-      navbar.style.display = "flex";
-    }
-  });
-  
-  
+ham_icon.addEventListener("click", ()=> {
+  if (navbar.style.display === "flex") {
+    navbar.style.display = "none";
+  } else {
+    navbar.style.display = "flex";
+  }
+});
+
+window.addEventListener("resize", () => {
+  if (window.innerWidth > 800) {
+    navbar.style.display = "flex";
+  } else {
+    navbar.style.display = "none";
+  }
+});
